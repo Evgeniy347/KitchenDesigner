9eceba4df7d831952f33d3e41c6f62cd45ff8da0
Маркин Евгений <emarkin@wss-consulting.ru>
2026-07-11 20:04:00 +0500

    fix: persistent PostgreSQL container + DB init retry logic

--- server/KitchenServer.AppHost/AppHost.cs
+++ server/KitchenServer.AppHost/AppHost.cs
@@ -4,6 +4,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var postgres = builder.AddPostgres("postgres")
     .WithPgAdmin()
+    .WithLifetime(ContainerLifetime.Persistent)
     .AddDatabase("kitchendb");
 
 builder.AddProject<KitchenServer_Web>("web")
