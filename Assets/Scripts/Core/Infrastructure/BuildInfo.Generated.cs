d4c576107eabba70bc01896178e6924107d9db10
Маркин Евгений <emarkin@wss-consulting.ru>
2026-07-13 20:38:33 +0500

    chore: add BuildInfo.Generated.cs to gitignore

--- Assets/Scripts/Core/Infrastructure/BuildInfo.Generated.cs
+++
@@ -1,8 +0,0 @@
-namespace KitchenDesigner.Core
-{
-    public static partial class BuildInfo
-    {
-        public const string Version = "0.0";
-        public const string BuildDate = "—";
-    }
-}
