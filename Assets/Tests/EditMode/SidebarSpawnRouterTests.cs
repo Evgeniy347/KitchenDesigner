a1b73383c9a9221d7b6c17cb43da33d7900370c9
Маркин Евгений <emarkin@wss-consulting.ru>
2026-09-03 04:37:18 +0500

    fix: teach the sidebar router test double about the bathtub spawn

--- Assets/Tests/EditMode/SidebarSpawnRouterTests.cs
+++ Assets/Tests/EditMode/SidebarSpawnRouterTests.cs
@@ -82,6 +82,8 @@ public class SidebarSpawnRouterTests
 
         public void SpawnWallHungToilet(string name) => Put(nameof(SpawnWallHungToilet), name);
 
+        public void SpawnBathtub(Vector3Int dims, string name) => Put(nameof(SpawnBathtub), dims, name);
+
         public void SpawnPanel(Vector3Int dims, string name,
             int gapLeft, int gapRight, int gapTop, int gapBottom) =>
             Put(nameof(SpawnPanel), dims, name, gapLeft, gapRight, gapTop, gapBottom);
